d7d1422418c747831cc691e29c27328ac646ca3d
Rajlaxmi-Mane <maithilimanepatil0424@gmail.com>
2025-01-09 11:46:39 +0530

    Add files via upload
    API call in .net c#

---
+++ APICall/APICall/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APICall.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> students = new List<Student>();
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.id == id);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] Student student)
+        {
+            students.Add(student);
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Student student)
+        {
+           int i= students.FindIndex(s => s.id == id);
+            if(i>=0)
+            {
+                students[i] = student;
+            }
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+           students.RemoveAll(s => s.id == id);
+        }
+    }
+}
